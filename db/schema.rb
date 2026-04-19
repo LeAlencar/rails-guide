c582f0cf94c67b04f9a88f3f4a61b243b59a492f
Leandro Alencar <zzleandrobritozz@gmail.com>
2026-04-18 21:57:41 -0300

    feat: add signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_19_201644) do
+ActiveRecord::Schema[8.0].define(version: 2026_04_19_004550) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -78,6 +78,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_19_201644) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
